4a11b4f33edbefb0cd77aca5ef43003d2edfff9b
NabeelShaikh12 <qaziabdulrahmannt@gmail.com>
2025-01-08 23:51:50 +0500

    my updated code

---
+++ src/pages/steps/step.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BG from "../../assets/img/hero/hero3/h19-1.jpg";
+
+
+export const Step = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3" style={{border: 'solid 1px'}}>
+
+                </div>
+                <div className="col-md-3">
+
+                </div>
+                <div className="col-md-3">
+
+                </div>
+                <div className="col-md-3">
+                
+                </div>
+            </div>
+        </div>
+    )
+
+}
